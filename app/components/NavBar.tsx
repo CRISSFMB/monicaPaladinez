0f1a31d72911d83a13e82ea8eee9acb6085a5a9f
cristian mosquera <cmosqbone1@gmail.com>
2025-03-26 01:55:46 +0100

    agregando navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -6,10 +6,10 @@ const NavBar = () => {
     return (
         <nav className={Style.backgroundNav}>
             <div className={`container ${Style.nav}`}>
-                <Link href={"/"} className={Style.logo}>Monica Paladinez</Link>
+                <Link href={"/"} className={Style.logo}>Monica Paladinez | </Link>
                 <ul className={Style.navLinks}>
                     <li>
-                        <Link className={Style.link} href={"/"}>Home</Link>
+                        <Link className={Style.link} href={"/"}>Home | </Link>
                     </li>
                     <li>
                         <Link className={Style.link} href={"/about"}>About</Link>
