b42282b5d4ab31d271021431b57b4184ad5fe6d5
cristian mosquera <cmosqbone1@gmail.com>
2025-03-26 01:53:17 +0100

    cambio de color dorado

--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -2,17 +2,9 @@
 import Image from "next/image";
 import NetworkComponent from "../networkComponents/NetworkComponent";
 import styles from "./Hero.module.css";
-
-// import NetworkComponent from "../networkComponents/NetworkComponent";
-
-
-
 const Hero = () => {
-
-
   return (
     <div className={`${styles.hero} container `}>
-
       <div className={styles["hero__content"]}>
         <Image
           src="/logos/logoMonica.webp"
@@ -23,11 +15,8 @@ const Hero = () => {
         />
         <h1 className={styles["hero__content__title"]}><span className={styles["hero__content__titleSpan"]}>M</span>onica <span className={styles["hero__content__titleSpan"]}>P</span>aladinez |  20 <span className={styles["hero__content__titleSpan"]}>+</span> Years Of Real Estate Experience<span className={styles["hero__content__titleSpan"]}>.</span></h1>
         <p className={styles["hero__content__paragraph"]}>With Me at your side, managing your Orlando properties becomes a hassle-free task</p>
-
         <NetworkComponent />
       </div>
-
-
     </div>
   );
 };
